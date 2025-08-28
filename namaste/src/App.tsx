d9ba24c85ca87382a010aa8837871457a80968c0
RamanT <149133866+DevolRaman750@users.noreply.github.com>
2025-08-28 20:18:13 +0530

    ShadCN,SignUp

--- namaste/src/App.tsx
+++ namaste/src/App.tsx
@@ -2,21 +2,30 @@ import React from 'react'
 import'./globals.css'
 import { Routes,Route } from 'react-router-dom'
 import SignInForm from './_auth/forms/SignInForm'
-import HomePage from './root/pages/HomePage'
+
 import SignUpForm from './_auth/forms/SignUpForm'
+import { HomePage } from './root/pages'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './root/RootLayout'
 
 const App = () => {
   return (
    <main className='flex h-screen'>
     <Routes>
         {/* public routes -like login and signup*/ }
+
+        <Route element={<AuthLayout/>}>
+        
           <Route path='/sign-in' element={<SignInForm/>}/>
           <Route path='/sign-up' element={<SignUpForm/>}/>
+        </Route>
           
 
 
         {/* private routes after login and signup*/}
-        <Route index element={<HomePage/>}/>
+        <Route element={<RootLayout/>}>
+          <Route index element={<HomePage/>}/>
+        </Route>
 
 
     </Routes>
